de4eaba56c3a2a5a2f3adc964c2ba7ffd930a394
asfarmer33 <148174736+asfarmer33@users.noreply.github.com>
2023-11-08 11:47:19 -0500

    better movement

--- player.py
+++ player.py
@@ -1,17 +1,32 @@
 import pygame
+import math
 
 class Player(pygame.sprite.Sprite):
 
     def __init__(self, screen, image, x = 0, y = 0):
         self.screen = screen
         self.image = pygame.image.load(image)
+        self.rot_image = self.image
         self.x = x
         self.y = y
-        self.speed = 1
+        self.rotate = 0
+        self.speed = 0
+        self.angle = 0
+        self.rect = self.image.get_rect()
+        self.rect.centerx = self.x
+        self.rect.centery = self.y
 
     def draw(self):
-        self.screen.blit(self.image, (self.x, self.y))
+        self.screen.blit(self.rot_image, self.rect)
     def update(self):
-        self.x += self.speed
-        self.y += self.speed
+        if (self.x - self.rect.width/2) > 0 and (self.x + self.rect.width/2) < self.screen.get_width():
+            self.x += self.speed * math.cos(math.pi/2 - self.angle*math.pi/180)
+        if (self.y - self.rect.height/2) > 0 and (self.y + self.rect.height/2) < self.screen.get_height():
+            self.y += self.speed * math.sin(math.pi/2 - self.angle*math.pi/180)
+        self.rect.centerx = self.x
+        self.rect.centery = self.y
+
+    def turn(self):
+        self.rot_image = pygame.transform.rotate(self.image, self.angle)
+        self.rect = self.rot_image.get_rect(center = (self.x, self.y))
 
--- tank_game.py
+++ tank_game.py
@@ -12,7 +12,7 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 background = make_background(screen)
 
-player_tank = Player(screen, "images/tank_blue.png")
+player_tank = Player(screen, "images/tank_blue.png", 100, 100)
 
 
 
@@ -22,11 +22,31 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-        if event.type == pygame.KEYDOWN:
+
+    keys = pygame.key.get_pressed() # list of pressed keys
+    if keys[pygame.K_UP]:
+        player_tank.speed = 1
+    elif keys[pygame.K_DOWN]:
+        player_tank.speed = -1
+    else:
+        player_tank.speed = 0
+    if keys[pygame.K_LEFT]:
+        player_tank.angle += 2
+        player_tank.turn()
+    elif keys[pygame.K_RIGHT]:
+        player_tank.angle -= 2
+        player_tank.turn()
+        '''if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
                 player_tank.speed += 1
             if event.key == pygame.K_DOWN:
                 player_tank.speed -= 1
+            if event.key == pygame.K_LEFT:
+                player_tank.angle += 10
+                player_tank.turn()
+            if event.key == pygame.K_RIGHT:
+                player_tank.angle -= 10
+                player_tank.turn()'''
 
     screen.blit(background, (0, 0))
 
